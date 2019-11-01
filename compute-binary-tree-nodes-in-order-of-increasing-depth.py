a0ba60d98eb1d8a669eaa4ff24aa25a636f012fe
Andy W <Andy.Kee.Wong@gmail.com>
2019-10-31 20:53:42 -0400

    8.6

---
+++ compute-binary-tree-nodes-in-order-of-increasing-depth.py
@@ -0,0 +1,28 @@
+"""
+Binary trees are formally defined in Chapter 9. In particulaar, each node in a
+binary tree has a depth, which is its distance from the root.
+
+Given a binary tree, return an array consisting of the keys at the same level.
+Keys should appear in the order of the corresponding nodes' depths, breaking
+ties from left to right. For example, you should return
+<<314>,<6,6>,<271,561,2,271>,<28,0,3,1,28>,<17,401,257>,<641>> for the binary
+tree in Figure 9.1 on Page 112.
+
+Hint: First think about solving this problem with a pair of queues.
+
+"""
+
+def binary_tree_depth_order(tree):
+    result = []
+    if not tree:
+        return result
+
+    curr_depth_nodes = [tree]
+    while curr_depth_nodes:
+        result.append([curr.data for curr in curr_depth_nodes])
+        curr_depth_nodes = [
+            child
+            for curr in curr_depth_nodes for child in (curr.left, curr.right)
+            if child
+        ]
+    return result
